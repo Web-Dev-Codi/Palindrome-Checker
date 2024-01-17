6442f49bd55cfd442255cba1d5a3ba77dc6cf422
Web-Dev-Codi <cordiscobrian@gmail.com>
2024-01-17 10:14:30 +0100

    fix(app): Testing App

--- script.js
+++ script.js
@@ -18,20 +18,32 @@ const _$palindromCheckerFn = (str) => {
 			.toString()
 			.replace(/[\s~`!@#$%^&*(){}\[\];:"'<,.>?\/\\|_+=-]/g, "");
 		if (arrStr !== arrStrTwo) {
+			result.style.display = "block";
 			console.log(`${str} is NOT a palindrome`);
 		}
 		if (arrStr === arrStrTwo) {
+			result.style.display = "block";
 			console.log(`${str} is a palindrome`);
 		}
 	}
 };
 
+const deleteCloseElement = (event) => {
+	result.style.display = 'none'
+}
+
 const checkBtn = (event) => {
 	event.preventDefault();
-	result.style.display = "block";
-	textParser()
+	if (!textInput.value) {
+		alert("Please input a value");
+		result.style.display = "none";
+	} else if (textInput.value) {
+		result.style.display = "block";
+	} 
+	textInput.value = "";
+	_$palindromCheckerFn(event.target.value)
 };
-
 
 textInput.addEventListener("input", textParser);
 checkButton.addEventListener("click", checkBtn);
+textInput.addEventListener("keydown", deleteCloseElement);
