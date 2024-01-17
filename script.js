e6ff360d7804f60317ddbf9c419c97206d999f39
Web-Dev-Codi <cordiscobrian@gmail.com>
2024-01-17 09:11:21 +0100

    perf(app): Palindrome FN g2g
    Palindrome functionality 95% complete

--- script.js
+++ script.js
@@ -1,47 +1,37 @@
-// const textInput = document.getElementById("text-input");
-// const checkButton = document.getElementById("check-btn");
-// const result = document.getElementById("result");
+const textInput = document.getElementById("text-input");
+const checkButton = document.getElementById("check-btn");
+const result = document.getElementById("result");
 const hiddenContainer = document.getElementById("hidden-container");
 
-// const textParser = (event) => {
-// 	result.innerText = event.target.value;
-// };
-
-const arrayStr = ["racecar"];
-const arrayStr2 = ["love"];
-
+const textParser = (event) => {
+	result.innerText = event.target.value;
+};
 
 const _$palindromCheckerFn = (str) => {
-	const answer = "";
-
 	for (let i = 0; i < str.length; i++) {
-		for (let j = 0; j < str.length; j++) {
-			let one = [...str[i]];
-			let two = [...str[j]].reverse();
-			if (JSON.stringify(one) === JSON.stringify(two)) {
-				console.log(`${str} is a palindrome`);
-			} else if (JSON.stringify(one) !== JSON.stringify(two)) {
-				console.log(`${str} is NOT a palindrome`);
-			}
+		const arrOne = [...str[i]];
+		const arrTwo = [...arrOne].reverse();
+		const arrStr = arrOne
+			.toString()
+			.replace(/[\s~`!@#$%^&*(){}\[\];:"'<,.>?\/\\|_+=-]/g, "");
+		const arrStrTwo = arrTwo
+			.toString()
+			.replace(/[\s~`!@#$%^&*(){}\[\];:"'<,.>?\/\\|_+=-]/g, "");
+		if (arrStr !== arrStrTwo) {
+			console.log(`${str} is NOT a palindrome`);
+		}
+		if (arrStr === arrStrTwo) {
+			console.log(`${str} is a palindrome`);
 		}
 	}
 };
 
-console.log(palindromChecker(arrayStr));
-console.log(palindromChecker(arrayStr2));
+const checkBtn = (event) => {
+	event.preventDefault();
+	result.style.display = "block";
+	textParser()
+};
 
-// const checkBtn = (event) => {
-// 	event.preventDefault();
-// 	palindromChecker(textParser);
-// };
 
-// const resultsDiv = (textParser) => {
-// 	const displayDiv = document.createElement("p");
-// 	const text = document.createTextNode(textParser);
-//     result.appendChild(displayDiv);
-// 	displayDiv.appendChild(text)
-// 	palindromChecker(textParser)
-// };
-
-// textInput.addEventListener("input", textParser);
-// checkButton.addEventListener("click", checkBtn);
+textInput.addEventListener("input", textParser);
+checkButton.addEventListener("click", checkBtn);
