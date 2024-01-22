a0d04cb2a21b5845e5c76d61b293f1d01667a52a
Web-Dev-Codi <cordiscobrian@gmail.com>
2024-01-22 16:07:57 +0100

    fix(app): Palindrom Success

--- script.js
+++ script.js
@@ -3,58 +3,51 @@ const checkButton = document.getElementById("check-btn");
 const result = document.getElementById("result");
 const resetBtn = document.getElementById("reset-btn");
 
-const _$palindromCheckerFn = (str) => {
+function isPalindrome(str) {
 	const para = document.createElement("p");
-	let strOne = "";
-	let strTwo = "";
-	for (let i = 0; i < str.length; i++) {
-		const strArray = Array.from(str[i]);
-		const strArrayTwo = Array.from(strArray).slice().reverse();
-		let newOne = strArray
-			.toString()
-			.toLowerCase()
-			.replace(/[\s~`!@#$%^&*(){}\[\];:"'<,.>?\/\\|_+=-]/g, "");
-		let newTwo = strArrayTwo
-			.toString()
-			.toLowerCase()
-			.replace(/[\s~`!@#$%^&*(){}\[\];:"'<,.>?\/\\|_+=-]/g, "");
-
-		if (newOne !== newTwo) {
-			result.appendChild(para);
-			result.style.display = "block";
-			para.className = "user-input";
-			strTwo = `${str} is NOT a palindrom`;
-			para.innerText = strTwo;
-
-		}
-		if (newOne === newTwo) {
-			result.appendChild(para);
-			result.style.display = "block";
-			para.className = "user-input";
-			strOne = `${str} is a palindrom`;
-			para.innerText = strOne;
-		
-		}
-	}
-};
+	let formattedStr = str.toLowerCase().replace(/[^a-z0-9]/g, "");
+	let reversedStr = formattedStr.split("").reverse().join("");
+			if (formattedStr === reversedStr) {
+				para.className = "user-input";
+				result.appendChild(para);
+				result.style.display = "block";
+				formattedStr = `${str} is a palindrom`;
+				console.log(`${str} is a palindrom`);
+				para.innerText = formattedStr;
+			} else {
+				para.className = "user-input";
+				result.appendChild(para);
+				result.style.display = "block";
+				reversedStr = `${str} is NOT a palindrom`;
+				console.log(`${str} is NOT a palindrom`);
+				para.innerText = reversedStr;
+			}
+	return formattedStr === reversedStr;
+}
 
 
-const resetUI = (event) => {	
-	textInput.value = "";
-};
-
-
-const checkBtn = (event) => {
+function inputValue() {
 	const str = textInput.value;
-	if (!str) {
-		alert("Please input a value");
-		result.style.display = "none";
-	}
-	if (str) {
-		resetUI();
-	}
-	_$palindromCheckerFn(str);
-};
+	return str
 
+}
+
+function resetUI(event) {
+	textInput.value = "";
+}
+
+function checkBtn(str) {
+	if (!textInput.value) {
+		alert("Please input a value");
+	}
+	if (textInput.value) {
+		const str = textInput.value;
+		isPalindrome(textInput.value);
+		return str;
+	}
+
+}
+
+textInput.addEventListener("input", inputValue);
 checkButton.addEventListener("click", checkBtn);
 resetBtn.addEventListener("click", resetUI);
