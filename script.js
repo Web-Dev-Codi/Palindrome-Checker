d160bc00e4437e5cec240cc0cc28ef372ca23821
Web-Dev-Codi <cordiscobrian@gmail.com>
2024-01-22 17:37:06 +0100

    Getting closer

--- script.js
+++ script.js
@@ -7,47 +7,50 @@ function isPalindrome(str) {
 	const para = document.createElement("p");
 	let formattedStr = str.toLowerCase().replace(/[^a-z0-9]/g, "");
 	let reversedStr = formattedStr.split("").reverse().join("");
-			if (formattedStr === reversedStr) {
-				para.className = "user-input";
-				result.appendChild(para);
-				result.style.display = "block";
-				formattedStr = `${str} is a palindrom`;
-				console.log(`${str} is a palindrom`);
-				para.innerText = formattedStr;
-			} else {
-				para.className = "user-input";
-				result.appendChild(para);
-				result.style.display = "block";
-				reversedStr = `${str} is NOT a palindrom`;
-				console.log(`${str} is NOT a palindrom`);
-				para.innerText = reversedStr;
-			}
+	if (formattedStr === reversedStr) {
+		para.className = "user-input";
+		result.appendChild(para);
+		result.style.display = "block";
+		formattedStr = `${str} is a palindrom`;
+		console.log(`${str} is a palindrom`);
+		para.innerText = formattedStr;
+	} else {
+		para.className = "user-input";
+		result.appendChild(para);
+		result.style.display = "block";
+		reversedStr = `${str} is NOT a palindrom`;
+		console.log(`${str} is NOT a palindrom`);
+		para.innerText = reversedStr;
+	}
 	return formattedStr === reversedStr;
 }
 
-
 function inputValue() {
 	const str = textInput.value;
-	return str
-
+	return str;
 }
 
 function resetUI(event) {
 	textInput.value = "";
+	checkButton.className = "palindrom-btn";
+	result.remove();
+	resetBtn.style.display = 'none'
+	
 }
 
-function checkBtn(str) {
+function checkBtn(event) {
 	if (!textInput.value) {
 		alert("Please input a value");
 	}
 	if (textInput.value) {
 		const str = textInput.value;
+		checkButton.style.display = "none";
+		resetBtn.className = "palindrome-reset-btn";
 		isPalindrome(textInput.value);
 		return str;
 	}
-
 }
 
+resetBtn.addEventListener("click", resetUI);
 textInput.addEventListener("input", inputValue);
 checkButton.addEventListener("click", checkBtn);
-resetBtn.addEventListener("click", resetUI);
